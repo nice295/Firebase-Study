7bf0084ba64ab01df3b2d10ad5f88d27ccabd227
nice295 <nice295@gmail.com>
2016-06-21 22:56:40 +0900

    02.Get set string - get set complete

--- 01-Get set string/FirebaseStudy/app/src/main/java/com/nice295/firebasestudy/MainActivity.java
+++ 01-Get set string/FirebaseStudy/app/src/main/java/com/nice295/firebasestudy/MainActivity.java
@@ -5,23 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.common.SignInButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference myRef = database.getReference("number");
 
     private TextView mTextEmotion;
-    private Button mButtonMinus;
-    private Button mButtonPlus;
+    private Button mButtonHappy;
+    private Button mButtonGloomy;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +33,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Views
         mTextEmotion = (TextView) findViewById(R.id.textEmotion);
-        mButtonMinus = (Button) findViewById(R.id.buttonHappy);
-        mButtonPlus = (Button) findViewById(R.id.buttonGloomy);
+        mButtonHappy = (Button) findViewById(R.id.buttonHappy);
+        mButtonGloomy = (Button) findViewById(R.id.buttonGloomy);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         // Click listeners
-        mButtonMinus.setOnClickListener(this);
-        mButtonPlus.setOnClickListener(this);
+        mButtonHappy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("Happy");
+            }
+        });
+        mButtonGloomy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("Gloomy");
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        mProgressBar.setVisibility(View.VISIBLE);
+
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+                mProgressBar.setVisibility(View.INVISIBLE);
+
                 String value = dataSnapshot.getValue(String.class);
                 Log.d(TAG, "Value is: " + value);
                 mTextEmotion.setText(value);
@@ -59,10 +75,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 }
