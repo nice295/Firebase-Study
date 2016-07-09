83e7da65f8b2f98684bafa545d1f870430ee5421
nice295 <nice295@gmail.com>
2016-07-10 00:32:59 +0900

    add dialog

---
+++ 01.String with dialog/FirebaseStudy/app/src/main/java/com/nice295/firebasestudy/MainActivity.java
@@ -0,0 +1,109 @@
+package com.nice295.firebasestudy;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = database.getReference("string-dialog");
+
+    private TextView mTextEmotion;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Views
+        mTextEmotion = (TextView) findViewById(R.id.textEmotion);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                mTextEmotion.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        showAddDialog();
+        return true;
+    }
+
+    private void showAddDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this, R.style.DialogTheme);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_add, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText etNewString = (EditText) dialogView.findViewById(R.id.edit);
+
+        dialogBuilder.setTitle(getString(R.string.add_new_string));
+        //dialogBuilder.setMessage("");
+        dialogBuilder.setPositiveButton(getString(R.string.add), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (etNewString.getText() != null) {
+                    myRef.setValue(etNewString.getText().toString());
+                    etNewString.clearFocus();
+                    etNewString.setText("");
+                } else {
+                    Log.e(TAG, "Input Text!");
+                }
+            }
+        });
+        dialogBuilder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+}
